4692d4b2cdae2a14da046478a01e8ddd76cca1ea
Wendulka <wendulka@example.com>
2025-12-18 18:44:37 +0100

    Add console version log and fix main.tsx syntax

--- broker-client/src/main.tsx
+++ broker-client/src/main.tsx
@@ -1,12 +1,14 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FluentProvider, webLightTheme } from '@fluentui/react-components'
-import App from './App'
+import App from './App.tsx'
 import { TranslationProvider } from './context/TranslationContext'
 import './index.css'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+console.log(`Investyx App Version: ${__APP_VERSION__} (${__APP_BUILD_DATE__})`);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FluentProvider theme={webLightTheme}>
       <TranslationProvider>
